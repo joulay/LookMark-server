70ce4e70310161b5e2ecc94405e4f8f3e5020194
Julie K <julieskim160@gmail.com>
2018-04-24 11:00:32 -0700

    auth route, thanks wence

---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config');
+
+
+const options = {session: false, failWithError: true};
+
+const localAuth = passport.authenticate('local', options);
+
+function createAuthToken (user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+  });
+}
+
+
+router.post('/login', localAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  return res.json({ authToken });
+});
+
+const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
+
+router.post('/refresh', jwtAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  res.json({ authToken });
+});
+
+
+module.exports = router;
