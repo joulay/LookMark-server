4e381f57553b422ea3cec248faf7adbdc85f998f
Julie K <Home@Julies-iMac.local>
2018-04-26 09:08:36 -0700

    add to mlab

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/capstone',
+        process.env.DATABASE_URL || 'mongodb://jules:jules@ds159129.mlab.com:59129/jk-capstone',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test',
