e9b2e970a961dec2f3fd212ea66def4641aff3d8
Julie K <julieskim160@gmail.com>
2018-04-30 12:41:35 -0700

    install path

--- index.js
+++ index.js
@@ -55,6 +55,20 @@ function runServer(port = PORT) {
     });
 }
 
+app.post('/upload', (req, res, next) => {
+  console.log(req);
+  let imageFile = req.files.file;
+
+  imageFile.mv(`${__dirname}/public/${req.body.filename}.jpg`, function(err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    res.json({file: `public/${req.body.filename}.jpg`});
+  });
+
+})
+
 if (require.main === module) {
   dbConnect();
   runServer();
