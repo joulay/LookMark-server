e786862c8096c905980a9a78bc27105c9f5f2a4c
Julie K <julieskim160@gmail.com>
2018-05-02 21:04:53 -0700

    bride test up connect and drop db

--- test/bride.test.js
+++ test/bride.test.js
@@ -5,10 +5,10 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
-const {TEST_DATABASE_URL} = require('../config');
+const {TEST_DATABASE_URL, JWT_SECRET} = require('../config');
 const {dbConnect, dbDisconnect} = require('../db-mongoose');
-// const {dbConnect, dbDisconnect} = require('../db-knex');
-
+const User = require('../models/user');
+const Bride = require('../models/bride');
 // Set NODE_ENV to `test` to disable http layer logs
 // You can do this in the command line, but this is cross-platform
 process.env.NODE_ENV = 'test';
@@ -19,10 +19,27 @@ process.stdout.write('\x1Bc\n');
 const expect = chai.expect;
 chai.use(chaiHttp);
 
+
+describe('Lookmark API - Brides', function () {
+    const user;
+    const bride;
+
+
 before(function() {
   return dbConnect(TEST_DATABASE_URL);
 });
 
+beforeEach(function () {
+    return Bride.ensureIndexes();
+  });
+
+  afterEach(function () {
+    return mongoose.connection.db.dropDatabase();
+  });
+
 after(function() {
   return dbDisconnect();
 });
+
+
+})
