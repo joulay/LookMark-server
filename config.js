bc2ba4dd55a59d72c2ee5035c85d35b56ea80f22
Elias Mason <eliasjmason@gmail.com>
2018-05-06 10:20:13 -0700

    Tweak db uris

--- config.js
+++ config.js
@@ -3,11 +3,10 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://jules:jules@ds159129.mlab.com:59129/jk-capstone',
+  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/lookmarq',
   TEST_DATABASE_URL:
-        process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test',
+    process.env.TEST_DATABASE_URL ||
+    'mongodb://localhost/thinkful-backend-test',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
